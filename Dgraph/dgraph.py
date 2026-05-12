c22a6408eec1e3a2001970e62bf3f4c077dfa19a
Rebeca Armenta <rebeca.armenta@iteso.mx>
2026-05-12 15:25:11 -0600

    add function run_query in dgraph.py

--- Dgraph/dgraph.py
+++ Dgraph/dgraph.py
@@ -2,3 +2,16 @@ import json
 from connect import get_dgraph_client
 
 client = get_dgraph_client()
+
+def run_query(query):
+    txn = client.txn()
+    try:
+        res = txn.query(query)
+        data = json.loads(res.json)
+        print(json.dumps(data, indent=2))
+    except Exception as e:
+        print(f"Error en la consulta: {e}")
+    finally:
+        txn.discard()
+
+        
