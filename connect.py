d15694e76816c4316b7bfc292ba6182434bd3372
Rebeca Armenta <rebeca.armenta@iteso.mx>
2026-05-12 13:55:06 -0600

    create function for Dgraph in connect.py

--- connect.py
+++ connect.py
@@ -1,4 +1,9 @@
 from pymongo import MongoClient
+import pydgraph
+
+def get_dgraph_client():
+    client_stub = pydgraph.DgraphClientStub('localhost:9080')
+    return pydgraph.DgraphClient(client_stub)
 
 def get_mongo_db():
     client = MongoClient("mongodb://localhost:27017")
